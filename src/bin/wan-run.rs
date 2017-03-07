ce4c18b67f85b409a54d7a2eac0bcf34d800def9
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-03-08 05:25:16 +0900

    update

--- src/bin/wan-run.rs
+++ src/bin/wan-run.rs
@@ -1,23 +1,26 @@
-extern crate wan;
+#[macro_use]
 extern crate clap;
 extern crate env_logger;
+extern crate wan;
 
 fn main() {
-  let app = clap::App::new("wan-run")
-    .arg(clap::Arg::with_name("compiler").required(true).takes_value(true))
-    .arg(clap::Arg::with_name("filename").required(true).takes_value(true))
-    .arg(clap::Arg::with_name("arguments").required(false).takes_value(true).multiple(true));
-  let matches = app.get_matches();
-  let compiler = matches.value_of("compiler").unwrap();
-  let filename = matches.value_of("filename").unwrap();
-  let arguments: Vec<_> = matches.values_of("arguments").map(|v| v.collect()).unwrap_or_default();
-
   env_logger::init().unwrap();
+
+  let m = app_from_crate!()
+    .arg_from_usage("<compiler>       'compiler name'")
+    .arg_from_usage("<filename>       'target filename'")
+    .arg_from_usage("[<arguments>...] 'supplemental arguments to pass compiled binary'")
+    .get_matches();
+  let compiler = m.value_of("compiler").unwrap();
+  let filename = m.value_of("filename").unwrap();
+  let arguments: Vec<_> = m.values_of("arguments").map(|v| v.collect()).unwrap_or_default();
+
   let response = wan::compile_request(compiler, &filename, &arguments).unwrap();
   if let Some(message) = response.program_message {
     println!("{}", message);
   } else {
     println!("{}", response.compiler_message.unwrap());
   }
+
   std::process::exit(response.status);
 }
