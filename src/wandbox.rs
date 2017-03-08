d5ed37a9d406b906981b07e40c5dd238edf2a210
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-03-08 18:20:35 +0900

    wip: deserialize

--- src/wandbox.rs
+++ src/wandbox.rs
@@ -1,4 +1,5 @@
 use curl::easy::{Easy, List};
+use serde;
 use serde_json;
 use super::Result;
 
@@ -94,22 +95,137 @@ pub struct CompilerInfo {
   switches: Vec<CompilerSwitch>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct CompilerSwitch {
-  default: bool,
-  name: String,
-  #[serde(rename = "display-name")]
-  display_name: String,
-  #[serde(rename = "display-flags")]
-  display_flags: String,
-  options: Vec<CompilerOption>,
+  #[serde(deserialize_with = "bool_or_string")]
+  default: BoolOrString,
+
+  #[serde(deserialize_with = "one_or_more")]
+  options: Vec<CompilerOption>, // array of CompilerOption or CompilerOption
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, PartialEq)]
+pub enum BoolOrString {
+  Bool(bool),
+  Str(String),
+}
+
+fn bool_or_string<D>(d: D) -> ::std::result::Result<BoolOrString, D::Error>
+  where D: serde::Deserializer
+{
+  struct BoolOrStringVisitor;
+  impl serde::de::Visitor for BoolOrStringVisitor {
+    type Value = BoolOrString;
+
+    fn expecting(&self, formatter: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+      formatter.write_str("bool or string")
+    }
+
+    fn visit_bool<E>(self, value: bool) -> ::std::result::Result<BoolOrString, E>
+      where E: serde::de::Error
+    {
+      Ok(BoolOrString::Bool(value))
+    }
+
+    fn visit_str<E>(self, value: &str) -> ::std::result::Result<BoolOrString, E>
+      where E: serde::de::Error
+    {
+      Ok(BoolOrString::Str(value.to_owned()))
+    }
+  }
+
+  d.deserialize(BoolOrStringVisitor)
+}
+
+fn one_or_more<T, D>(d: D) -> ::std::result::Result<Vec<T>, D::Error>
+  where T: serde::Deserialize,
+        D: serde::Deserializer
+{
+  struct OneOrMoreVisitor<T>(::std::marker::PhantomData<T>);
+
+  impl<T> serde::de::Visitor for OneOrMoreVisitor<T>
+    where T: serde::de::Deserialize
+  {
+    type Value = Vec<T>;
+
+    fn expecting(&self, formatter: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+      formatter.write_str("one or more")
+    }
+
+    fn visit_seq<V>(self, visitor: V) -> ::std::result::Result<Self::Value, V::Error>
+      where V: serde::de::SeqVisitor
+    {
+      serde::Deserialize::deserialize(serde::de::value::SeqVisitorDeserializer::new(visitor))
+    }
+
+    fn visit_map<M>(self, visitor: M) -> ::std::result::Result<Self::Value, M::Error>
+      where M: serde::de::MapVisitor
+    {
+      let value =
+        serde::Deserialize::deserialize(serde::de::value::MapVisitorDeserializer::new(visitor))?;
+      Ok(vec![value])
+    }
+  }
+
+  d.deserialize(OneOrMoreVisitor(::std::marker::PhantomData))
+}
+
+
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct CompilerOption {
   name: String,
   #[serde(rename = "display-name")]
   display_name: String,
   #[serde(rename = "display-flags")]
   display_flags: String,
+}
+
+
+#[test]
+fn test_compiler_switch_1() {
+  let src = r#"{
+    "default":"boost-1.55",
+    "options":[{
+      "name":"boost-nothing",
+      "display-flags":"",
+      "display-name":"Don't Use Boost"
+    },{
+      "name":"boost-1.47",
+      "display-flags":"-I/usr/local/boost-1.47.0/include",
+      "display-name":"Boost 1.47.0"
+    }]
+  }"#;
+  let dst = serde_json::from_str::<CompilerSwitch>(src).unwrap();
+
+  assert_eq!(dst.default, BoolOrString::Str("boost-1.55".to_owned()));
+  assert_eq!(dst.options,
+             [CompilerOption {
+                name: "boost-nothing".to_owned(),
+                display_name: "Don't Use Boost".to_owned(),
+                display_flags: "".to_owned(),
+              },
+              CompilerOption {
+                name: "boost-1.47".to_owned(),
+                display_name: "Boost 1.47.0".to_owned(),
+                display_flags: "-I/usr/local/boost-1.47.0/include".to_owned(),
+              }]);
+}
+
+#[test]
+fn test_compiler_switch_2() {
+  let src = r#"{
+    "default":true,
+    "name":"sprout",
+    "display-flags":"-I/usr/local/sprout",
+    "display-name":"Sprout"
+  }"#;
+  let dst = serde_json::from_str::<CompilerSwitch>(src).unwrap();
+
+  assert_eq!(dst.default, BoolOrString::Bool(true));
+  assert_eq!(dst.options,
+             [CompilerOption {
+                name: "sprout".to_owned(),
+                display_name: "Sprout".to_owned(),
+                display_flags: "-I/usr/local/sprout".to_owned(),
+              }]);
 }
