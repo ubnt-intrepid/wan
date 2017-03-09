f7f2449bcf681ab821c60beb0be252df2dfc254b
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-03-09 14:47:24 +0900

    update

--- src/list.rs
+++ src/list.rs
@@ -1,3 +1,5 @@
+#[cfg(test)]
+use serde_json;
 use Result;
 use util::Either;
 use http;
@@ -62,39 +64,34 @@ pub fn get_compiler_info() -> Result<Vec<CompilerInfo>> {
   http::get_json("http://melpon.org/wandbox/api/list.json", &[])
 }
 
-#[cfg(test)]
-mod tests {
-  use super::*;
-  use serde_json;
+#[test]
+fn test_compiler_info() {
+  let src = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/list.json"));
+  let dst: Vec<CompilerInfo> = serde_json::from_str(src).unwrap();
+  println!("{:?}", dst);
+}
 
-  #[test]
-  fn test_compiler_info() {
-    let src = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/list.json"));
-    let dst: Vec<CompilerInfo> = serde_json::from_str(src).unwrap();
-    println!("{:?}", dst);
-  }
-
-  #[test]
-  fn test_compiler_switch() {
-    let src = r#"{
+#[test]
+fn test_compiler_switch() {
+  let src = r#"{
     "default":true,
     "name":"sprout",
     "display-flags":"-I/usr/local/sprout",
     "display-name":"Sprout"
   }"#;
-    let dst = serde_json::from_str::<Either<CompilerSwitch, CompilerSwitchMultiOptions>>(src)
-      .unwrap();
+  let dst = serde_json::from_str::<Either<CompilerSwitch, CompilerSwitchMultiOptions>>(src)
+    .unwrap();
 
-    let dst = dst.into_left().expect("invalid type");
-    assert_eq!(dst.default, true);
-    assert_eq!(dst.name, "sprout");
-    assert_eq!(dst.display_name, "Sprout");
-    assert_eq!(dst.display_flags, "-I/usr/local/sprout");
-  }
+  let dst = dst.into_left().expect("invalid type");
+  assert_eq!(dst.default, true);
+  assert_eq!(dst.name, "sprout");
+  assert_eq!(dst.display_name, "Sprout");
+  assert_eq!(dst.display_flags, "-I/usr/local/sprout");
+}
 
-  #[test]
-  fn test_compiler_switch_multi() {
-    let src = r#"{
+#[test]
+fn test_compiler_switch_multi() {
+  let src = r#"{
     "default":"boost-1.55",
     "options":[{
       "name":"boost-nothing",
@@ -106,21 +103,20 @@ mod tests {
       "display-name":"Boost 1.47.0"
     }]
   }"#;
-    let dst = serde_json::from_str::<Either<CompilerSwitch, CompilerSwitchMultiOptions>>(src)
-      .unwrap();
-    let dst = dst.into_right().unwrap();
+  let dst = serde_json::from_str::<Either<CompilerSwitch, CompilerSwitchMultiOptions>>(src)
+    .unwrap();
+  let dst = dst.into_right().unwrap();
 
-    assert_eq!(dst.default, "boost-1.55");
-    assert_eq!(dst.options,
-               [CompilerOption {
-                  name: "boost-nothing".to_owned(),
-                  display_name: "Don't Use Boost".to_owned(),
-                  display_flags: "".to_owned(),
-                },
-                CompilerOption {
-                  name: "boost-1.47".to_owned(),
-                  display_name: "Boost 1.47.0".to_owned(),
-                  display_flags: "-I/usr/local/boost-1.47.0/include".to_owned(),
-                }]);
-  }
+  assert_eq!(dst.default, "boost-1.55");
+  assert_eq!(dst.options,
+             [CompilerOption {
+                name: "boost-nothing".to_owned(),
+                display_name: "Don't Use Boost".to_owned(),
+                display_flags: "".to_owned(),
+              },
+              CompilerOption {
+                name: "boost-1.47".to_owned(),
+                display_name: "Boost 1.47.0".to_owned(),
+                display_flags: "-I/usr/local/boost-1.47.0/include".to_owned(),
+              }]);
 }
